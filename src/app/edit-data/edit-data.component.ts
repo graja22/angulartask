112c0df1426abf6b062271fb40336f42c7de5656
gouthamraja22 <gouthamraja22@gmail.com>
2019-08-17 14:56:19 +0530

    kill you

---
+++ src/app/edit-data/edit-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-data',
+  templateUrl: './edit-data.component.html',
+  styleUrls: ['./edit-data.component.css']
+})
+export class EditDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
